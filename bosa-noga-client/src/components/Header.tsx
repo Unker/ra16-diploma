0c5b71dc7e117fad9317948f63f3321fc9ecb6b9
unker <kfi.unknown@gmail.com>
2024-04-07 00:01:45 +0300

    add Header

--- bosa-noga-client/src/components/Header.tsx
+++ bosa-noga-client/src/components/Header.tsx
@@ -1,36 +1,40 @@
-import { NavLink, useNavigate } from 'react-router-dom';
-import { ABOUT_ROUTE, CATALOG_ROUTE, CONTACTS_ROUTE, MAIN_PAGE_ROUTE } from '../utils/consts';
-import "../index.css"
-import { Col, Container, Navbar, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import {
+  Col,
+  Container,
+  Image,
+  Nav,
+  Navbar,
+  Row,
+} from 'react-bootstrap';
+import {
+  ABOUT_ROUTE,
+  CATALOG_ROUTE,
+  CONTACTS_ROUTE,
+  MAIN_PAGE_ROUTE,
+} from '../utils/consts';
+import '../index.css';
 
 const Header = () => {
-  // const navigate = useNavigate()
-  <NavLink to={MAIN_PAGE_ROUTE} className="nav-link">Главная</NavLink>
+  <NavLink to={MAIN_PAGE_ROUTE} className="nav-link">Главная</NavLink>;
 
   return (
     <header>
-      <Container>
-        <Row>
-          <Col>
-            <Navbar.Brand>
-              <NavLink className="navbar-brand" to={MAIN_PAGE_ROUTE}>
-                <img src="./img/header-logo.png" alt="Bosa Noga" />
-              </NavLink>
-              <div className="collapse navbar-collapse" id="navbarMain">
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item active">
-                    <NavLink className="nav-link" to={MAIN_PAGE_ROUTE}>Главная</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to={CATALOG_ROUTE}>Каталог</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to={ABOUT_ROUTE}>О магазине</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to={CONTACTS_ROUTE}>Контакты</NavLink>
-                  </li>
-                </ul>
+      <Row>
+        <Col>
+          <Navbar className="navbar navbar-expand-sm navbar-light bg-light">
+            <Container>
+              <Navbar.Brand>
+                <NavLink className="navbar-brand" to={MAIN_PAGE_ROUTE}>
+                  <Image src="./img/header-logo.png" alt="Bosa Noga" />
+                </NavLink>
+              </Navbar.Brand>
+              <Nav className="mr-auto">
+                <NavLink className="nav-link" to={MAIN_PAGE_ROUTE}>Главная</NavLink>
+                <NavLink className="nav-link" to={CATALOG_ROUTE}>Каталог</NavLink>
+                <NavLink className="nav-link" to={ABOUT_ROUTE}>О магазине</NavLink>
+                <NavLink className="nav-link" to={CONTACTS_ROUTE}>Контакты</NavLink>
+
                 <div>
                   <div className="header-controls-pics">
                     <div data-id="search-expander" className="header-controls-pic header-controls-search"></div>
@@ -43,13 +47,13 @@ const Header = () => {
                     <input className="form-control" placeholder="Поиск" />
                   </form>
                 </div>
-              </div>
-            </Navbar.Brand>
-          </Col>
-        </Row>
-      </Container>
+              </Nav>
+            </Container>
+          </Navbar>
+        </Col>
+      </Row>
     </header>
   );
-}
+};
 
 export default Header;
