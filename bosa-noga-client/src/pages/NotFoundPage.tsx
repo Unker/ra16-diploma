a8a17c38f2b555864aca5a8e7e7698586cf572ef
unker <kfi.unknown@gmail.com>
2024-04-07 23:31:26 +0300

    css 404

--- bosa-noga-client/src/pages/NotFoundPage.tsx
+++ bosa-noga-client/src/pages/NotFoundPage.tsx
@@ -6,15 +6,20 @@ const NotFoundPage = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>Error 404. Page not found</h1>
-      <i
-        onClick={() => navigate(MAIN_PAGE_ROUTE)}
-        style={{ cursor: 'pointer' }}
-      >
-        На главную
-      </i>
-    </>
+    <section className="top-sales">
+      <h1 className="text-center">Error 404. Page not found</h1>
+      <div className="text-center">
+        <p>
+          Извините, такая страница не найдена!
+        </p>
+        <i
+          onClick={() => navigate(MAIN_PAGE_ROUTE)}
+          style={{ cursor: 'pointer' }}
+        >
+          Перейти на главную
+        </i>
+      </div>
+    </section>
   );
 };
 
