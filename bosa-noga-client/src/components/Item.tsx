c869a19df5d4a0db46417f4b7409089c99239377
unker <kfi.unknown@gmail.com>
2024-04-13 01:40:30 +0300

    preloader image card

--- bosa-noga-client/src/components/Item.tsx
+++ bosa-noga-client/src/components/Item.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { IItemShort } from './types';
 import { PRODUCT_ROUTE } from '../utils/consts';
+import Preloader from './Preloader/Preloader';
 
 interface ItemProps {
   item: IItemShort;
@@ -10,6 +11,12 @@ interface ItemProps {
 
 const Item: React.FC<ItemProps> = ({ item }) => {
   const navigate = useNavigate();
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+    console.log('load img ', item.id);
+  };
 
   const handleOrderClick = () => {
     navigate(`${PRODUCT_ROUTE}/${item.id}`);
@@ -20,13 +27,23 @@ const Item: React.FC<ItemProps> = ({ item }) => {
       className='mb-4 pt-2 catalog-item-card'
       style={{ width: '32%' }}
     >
-      <div className='d-flex flex-column h-100'>
-        <Card.Img
-          variant="top"
-          src={item.images[0]}
-          alt={item.title}
-          style={{ flex: '1', objectFit: 'contain' }}
-        />
+      <div className='d-flex flex-column h-100' style={{minHeight: '400px'}}>
+        {imageLoaded ? (
+          <Card.Img
+            variant="top"
+            src={item.images[0]}
+            alt={item.title}
+            style={{ flex: '1', objectFit: 'contain' }}
+            onLoad={handleImageLoad}
+          />
+        ) : (
+          <div
+            style={{ flex: '1', objectFit: 'contain' }}
+            className="position-relative top-50 start-50 translate-middle"
+          >
+            <Preloader />
+          </div>
+        )}
         <Card.Footer style={{ backgroundColor: 'inherit', border: 'none' }}>
           <Card.Title>{item.title}</Card.Title>
           <Card.Text>{item.price} руб.</Card.Text>
