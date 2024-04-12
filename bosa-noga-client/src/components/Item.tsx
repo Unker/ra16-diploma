dbd281eaff641028227e2401817a31eb4a0d108b
unker <kfi.unknown@gmail.com>
2024-04-13 01:14:06 +0300

    centered image

--- bosa-noga-client/src/components/Item.tsx
+++ bosa-noga-client/src/components/Item.tsx
@@ -16,19 +16,30 @@ const Item: React.FC<ItemProps> = ({ item }) => {
   };
 
   return (
-    <Card className='mb-4 pt-2 catalog-item-card' style={{ width: '32%' }}>
-      <Card.Img variant="top" src={item.images[0]} alt={item.title} />
-      <Card.Body>
-        <Card.Title>{item.title}</Card.Title>
-        <Card.Text>{item.price} руб.</Card.Text>
-        <Button
-          variant="outline-dark"
-          className="btn btn-light align-self-start "
-          onClick={handleOrderClick}
-        >
-          Заказать
-        </Button>
-      </Card.Body>
+    <Card
+      className='mb-4 pt-2 catalog-item-card'
+      style={{ width: '32%' }}
+    >
+      <div className='d-flex flex-column h-100'>
+        <Card.Img
+          variant="top"
+          src={item.images[0]}
+          alt={item.title}
+          style={{ flex: '1', objectFit: 'contain' }}
+        />
+        <Card.Footer style={{ backgroundColor: 'inherit', border: 'none' }}>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>{item.price} руб.</Card.Text>
+          <Button
+            variant="outline-dark"
+            className="btn btn-light align-self-start "
+            onClick={handleOrderClick}
+          >
+            Заказать
+          </Button>
+
+        </Card.Footer>
+      </div>
     </Card>
   );
 };
