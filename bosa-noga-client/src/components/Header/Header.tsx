8297a2e13fb3fca94d2cd2c64385a25464eeeca9
unker <kfi.unknown@gmail.com>
2024-04-13 22:38:36 +0300

    reducible navbar

--- bosa-noga-client/src/components/Header/Header.tsx
+++ bosa-noga-client/src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import {
+  Button,
   Col,
   Form,
   Image,
   Nav,
+  NavDropdown,
   Navbar,
+  Offcanvas,
   Row,
 } from 'react-bootstrap';
 import cn from 'classnames';
@@ -64,63 +67,82 @@ const Header = (): JSX.Element => {
 
   return (
     <header className='container'>
-      <Row>
-        <Col>
-          <Navbar className="navbar navbar-expand-sm navbar-light bg-light px-3">
-            <Navbar.Brand>
-              <NavLink className="navbar-brand" to={MAIN_PAGE_ROUTE}>
-                <Image src="./img/header-logo.png" alt="Bosa Noga" />
-              </NavLink>
-            </Navbar.Brand>
-            <div className="collapse navbar-collapse" id="navbarMain">
-              <Nav className="me-auto">
-                <NavLink className="nav-link" to={MAIN_PAGE_ROUTE}>Главная</NavLink>
-                <NavLink className="nav-link" to={CATALOG_ROUTE}>Каталог</NavLink>
-                <NavLink className="nav-link" to={ABOUT_ROUTE}>О магазине</NavLink>
-                <NavLink className="nav-link" to={CONTACTS_ROUTE}>Контакты</NavLink>
-              </Nav>
-              <div>
-                <div className={s.headerControlsPics}>
-                  <div
-                    data-id="search-expander"
-                    className={cn(s.headerControlsPic, s.headerControlsSearch)}
-                    onClick={handleSearchIconClick}
-                  >
-                  </div>
-                  <div
-                    className={cn(s.headerControlsPic, s.headerControlsCart)}
-                    onClick={() => navigate(CART_ROUTE)}
-                  >
-                    {(cartItemCount && cartItemCount > 0) ? (
-                      <div className={s.headerControlsCartFull}>{cartItemCount}</div>
-                    ) : (
-                      <></>
-                    )}
-                    <div className={s.headerControlsCartMenu}></div>
-                  </div>
-                </div>
-
-                {searchVisible && (
-                  <div>
-                    <Form
-                      data-id="search-form"
-                      className={`form-inline ${s.headerControlsSearchForm}`}
-                      onSubmit={handleSearchFormSubmit}
-                    >
-                      <Form.Control
-                        className={`${s.formСontrol}`}
-                        placeholder="Поиск"
-                        value={searchText}
-                        onChange={(e) => handleSearchInputChange(e.target.value)}
-                      />
-                    </Form>
-                  </div>
+      {/* <Row>
+        <Col> */}
+      <Navbar className="navbar-expand-sm navbar-light bg-light px-3">
+        <Navbar.Brand>
+          <NavLink className="navbar-brand" to={MAIN_PAGE_ROUTE}>
+            <Image src="./img/header-logo.png" alt="Bosa Noga" />
+          </NavLink>
+        </Navbar.Brand>
+        <div className="collapse navbar-collapse" id="navbarMain">
+          <Nav className={`me-auto ${s.navBarDisabler}`}>
+            <NavLink className="nav-link" to={MAIN_PAGE_ROUTE}>Главная</NavLink>
+            <NavLink className="nav-link" to={CATALOG_ROUTE}>Каталог</NavLink>
+            <NavLink className="nav-link" to={ABOUT_ROUTE}>О магазине</NavLink>
+            <NavLink className="nav-link" to={CONTACTS_ROUTE}>Контакты</NavLink>
+          </Nav>
+          <NavDropdown
+            title="Навигация"
+            id={`offcanvasNavbarDropdown`}
+            style={{display: 'none', }}
+            className={s.dropDownEnabler}
+          >
+            <NavDropdown.Item>
+              <NavLink className="nav-link" to={MAIN_PAGE_ROUTE}>Главная</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink className="nav-link" to={CATALOG_ROUTE}>Каталог</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink className="nav-link" to={ABOUT_ROUTE}>О магазине</NavLink>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <NavLink className="nav-link" to={CONTACTS_ROUTE}>Контакты</NavLink>
+            </NavDropdown.Item>
+          </NavDropdown>
+          <div>
+            <div className={s.headerControlsPics}>
+              <div
+                data-id="search-expander"
+                className={cn(s.headerControlsPic, s.headerControlsSearch)}
+                onClick={handleSearchIconClick}
+              >
+              </div>
+              <div
+                className={cn(s.headerControlsPic, s.headerControlsCart)}
+                onClick={() => navigate(CART_ROUTE)}
+              >
+                {(cartItemCount && cartItemCount > 0) ? (
+                  <div className={s.headerControlsCartFull}>{cartItemCount}</div>
+                ) : (
+                  <></>
                 )}
+                <div className={s.headerControlsCartMenu}></div>
               </div>
             </div>
-          </Navbar>
-        </Col>
-      </Row>
+
+            {searchVisible && (
+              <div>
+                <Form
+                  data-id="search-form"
+                  className={`form-inline ${s.headerControlsSearchForm}`}
+                  onSubmit={handleSearchFormSubmit}
+                >
+                  <Form.Control
+                    className={`${s.formСontrol}`}
+                    placeholder="Поиск"
+                    value={searchText}
+                    onChange={(e) => handleSearchInputChange(e.target.value)}
+                  />
+                </Form>
+              </div>
+            )}
+          </div>
+        </div>
+      </Navbar>
+      {/* </Col>
+      </Row> */}
     </header>
   );
 };
